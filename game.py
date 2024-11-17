c870b414959125dbba12567fed304edff877c6c6
Prosamo <s4lm0n.014@gmail.com>
2024-11-17 14:04:43 +0900

    Update game.py

--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 import pyxel
-import PyxelUniversalFont
+import PyxelUniversalFont as puf
+writer = puf.Writer("ipa_gothic.ttf")
 class App:
     def __init__(self):
         pyxel.init(256, 256, title="テスト")
@@ -11,6 +12,7 @@ class App:
     def draw(self):
         pyxel.cls(7)
         pyxel.elli(128, 32, 64, 96, 8)
+        writer.draw(32, 32, 'テスト', 16, 0)
         
 if __name__ == "__main__":
     App()
